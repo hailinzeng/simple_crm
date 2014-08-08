137e7ed561a198fc0ceb220adcc36a8149ede626
Star <137379612@qq.com>
2014-08-08 14:58:13 +0800

    去掉登陆成功提示

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -12,7 +12,6 @@ class SessionsController < ApplicationController
     @user = User.find_with_login(params[:login], params[:password])
     if @user
       set_current_user(@user)
-      flash[:success] = "登录成功啦,激情的一天又开始啦."
       redirect_back_or_default profile_index_path
     else
       flash[:error] = "亲，帐号或密码错误."
