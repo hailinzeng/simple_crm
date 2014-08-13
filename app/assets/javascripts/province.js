6df77c11fc1efe2ea6c70675b62bf196f9de78e0
Star <137379612@qq.com>
2014-08-13 12:54:40 +0800

    返回值可能为空

--- app/assets/javascripts/province.js
+++ app/assets/javascripts/province.js
@@ -27,26 +27,28 @@
         url: '/provinces/' + provinceId + '/cities',
         type: 'GET',
         success: function(resp){
-          var options='';
-          $.each(resp, function(i, value){
-            var name = value[1];
-            var text = value[0];
-            if(i == 0){
-              options += '<option value="'+ name +'" selected>' + text + '</option>';
+          if(resp != null){
+            var options = '';
+            $.each(resp, function(i, value){
+              var name = value[1];
+              var text = value[0];
+              if(i == 0){
+                options += '<option value="'+ name +'" selected>' + text + '</option>';
+              }else{
+                options += '<option value="'+ name +'">' + text + '</option>';
+              }
+            });
+            $('#cityId').empty().append(options);
+            // jquery mobile set select default value
+            var cityName;
+            if(resp.length > 0){
+              cityName = resp[0][0];
+              getMarkets(resp[0][1]);
             }else{
-              options += '<option value="'+ name +'">' + text + '</option>';
+              cityName = currentEL.siblings('span').text();
             }
-          });
-          $('#cityId').empty().append(options);
-          // jquery mobile set select default value
-          var cityName;
-          if(resp.length > 0){
-            cityName = resp[0][0];
-            getMarkets(resp[0][1]);
-          }else{
-            cityName = currentEL.siblings('span').text();
+            $('#cityId').siblings('span').html(cityName);
           }
-          $('#cityId').siblings('span').html(cityName);
         }
       });
     }
@@ -58,25 +60,27 @@
         url: '/cities/' + cityId + '/markets',
         type: 'GET',
         success: function(resp){
-          var options='';
-          $.each(resp, function(i, value){
-            var name = value[1];
-            var text = value[0];
-            if(i == 0){
-              options += '<option value="'+ name +'" selected>' + text + '</option>';
+          if(resp != resp){
+            var options='';
+            $.each(resp, function(i, value){
+              var name = value[1];
+              var text = value[0];
+              if(i == 0){
+                options += '<option value="'+ name +'" selected>' + text + '</option>';
+              }else{
+                options += '<option value="'+ name +'">' + text + '</option>';
+              }
+            });
+            
+            $('#marketId').empty().append(options);
+            var marketName;
+            if(resp.length > 0){
+              marketName = resp[0][0];
             }else{
-              options += '<option value="'+ name +'">' + text + '</option>';
+              marketName = '&nbsp;';
             }
-          });
-          
-          $('#marketId').empty().append(options);
-          var marketName;
-          if(resp.length > 0){
-            marketName = resp[0][0];
-          }else{
-            marketName = '&nbsp;';
+            $('#marketId').siblings('span').html(marketName);
           }
-          $('#marketId').siblings('span').html(marketName);
         }
       });
     }
